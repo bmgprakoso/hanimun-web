f36b9291037aec8391979dddfb4a3f6da967573d
Bambang Adi Prakoso <bambang.adi@ruma.co.id>
2019-10-15 21:04:28 +0700

    Gather indonesian airports

--- src/components/FlightSearchBar/index.js
+++ src/components/FlightSearchBar/index.js
@@ -2,16 +2,23 @@ import React from "react";
 import "./styles.scss";
 
 function FlightSearchBar(props) {
-  return (
+
+return (
     <div className="columns is-horizontal is-vcentered">
         <div className="column is-full">
             <div className="columns">
                 <div className="column">
                     <div className="control is-expanded has-icons-left">
-                        <input className="input" type="text" placeholder="From" />
-                        <span className="icon is-small is-left">
+                        <div className="select">
+                            <select>
+                                <option selected>Country</option>
+                                <option>Select dropdown</option>
+                                <option>With options</option>
+                            </select>
+                        </div>
+                        <div className="icon is-small is-left">
                             <i className="fas fa-plane-departure"></i>
-                        </span>
+                        </div>
                     </div>
                 </div>
                 <div className="column">
